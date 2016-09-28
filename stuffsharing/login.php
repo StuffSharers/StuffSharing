e54eed995dff77bf8235b02d317047b6d618d140
John Yong <whipermr5@gmail.com>
2016-09-28 22:34:24 +0800

    Store only uid in session; query database for username every time

--- stuffsharing/login.php
+++ stuffsharing/login.php
@@ -6,47 +6,66 @@ session_start();
 
 $success = false;
 
+$username = isset($_POST["username"]) ? $_POST["username"] : "";
+$password = isset($_POST["password"]) ? $_POST["password"] : "";
+
 if (isset($_SESSION["uid"])) {
-    $success = true;
+    try {
+        $stmt = $db->prepare("SELECT username FROM ss_user WHERE uid = :uid;");
+
+        $stmt->bindParam(':uid', $_SESSION["uid"], PDO::PARAM_INT);
+
+        $stmt->execute();
+        $result = $stmt->fetch();
+
+        if ($result == false) {
+            throw new Exception("Unknown uid in session");
+        }
+
+        $username = $result["username"];
+        $success = true;
+
+    } catch (Exception $e) {
+        $message = "We are unable to process your request. Please try again later.";
+        session_unset();
+        session_destroy();
+        session_start();
+    }
 
 } else {
-    $username = isset($_POST["username"]) ? $_POST["username"] : "";
-    $password = isset($_POST["password"]) ? $_POST["password"] : "";
-
     if (!isset($_POST["login_token"])) {
         $message = "Please enter a valid username/email and password";
 
     } elseif ($_POST["login_token"] != $_SESSION["login_token"]) {
         $message = "Invalid form submission";
 
+    } elseif (empty($username) || strlen($username) > 255 || strlen($password) > 20 || strlen($password) < 4) {
+        $message = "Access denied";
+
     } else {
-        if (empty($username) || strlen($username) > 255 || strlen($password) > 20 || strlen($password) < 4) {
-            $message = "Access denied";
+        $password = sha1($password);
 
-        } else {
-            $password = sha1($password);
+        try {
+            $stmt = $db->prepare("SELECT uid, username FROM ss_user
+                                  WHERE (username = :username OR email = :username) AND password = :password;");
 
-            try {
-                $stmt = $db->prepare("SELECT uid, username FROM ss_user WHERE (username = :username OR email = :username) AND password = :password;");
+            $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+            $stmt->bindParam(':password', $password, PDO::PARAM_STR, 40);
 
-                $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-                $stmt->bindParam(':password', $password, PDO::PARAM_STR, 40);
+            $stmt->execute();
+            $result = $stmt->fetch();
 
-                $stmt->execute();
-                $result = $stmt->fetch();
+            if ($result == false) {
+                $message = "Access denied";
 
-                if ($result == false) {
-                    $message = "Access denied";
-                } else {
-                    $_SESSION["uid"] = $result["uid"];
-                    $_SESSION["username"] = $result["username"];
-                    $success = true;
-                }
-
-            } catch (PDOException $e) {
-                $message = $e->getMessage();//"We are unable to process your request. Please try again later.";
+            } else {
+                $_SESSION["uid"] = $result["uid"];
+                $username = $result["username"];
+                $success = true;
             }
 
+        } catch (PDOException $e) {
+            $message = "We are unable to process your request. Please try again later.";
         }
 
     }
@@ -62,7 +81,7 @@ $_SESSION['login_token'] = $login_token;
 </head>
 <body>
 <?php if ($success): ?>
-    <p>Success! You are logged in as <b><?=$_SESSION["username"]?></b>. <a href="logout.php">Logout</a></p>
+    <p>Success! You are logged in as <b><?=$username?></b>. <a href="logout.php">Logout</a></p>
 <?php else: ?>
     <p><?=$message?></p>
     <form method="POST">
