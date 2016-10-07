732591c5c76590918322a6357ce6ee88d32fddcf
John Yong <whipermr5@gmail.com>
2016-10-07 17:23:06 +0800

    Extracted out head and navigation into partials

--- stuffsharing/index.php
+++ stuffsharing/index.php
@@ -13,85 +13,11 @@ try {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>StuffSharing | a place to share stuff</title>
-
-    <!-- jQuery -->
-    <script src="resources/js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="resources/js/bootstrap.min.js"></script>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="resources/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="resources/css/3-col-portfolio.css" rel="stylesheet">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="resources/css/font-awesome.min.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
+<?php include("partials/head.html") ?>
 
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="./">StuffSharing</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">Browse</a>
-                    </li>
-                    <li>
-                        <a href="#">Advertise</a>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-<?php if ($is_authed): ?>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i> <?=$username?> <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="editprofile.php"><i class="fa fa-cog" aria-hidden="true"></i> Edit Profile</a></li>
-                            <li><a href="logout.php?redirect=main"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
-                        </ul>
-                    </li>
-<?php else: ?>
-                    <li>
-                        <a href="login.php?redirect=main"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
-                    </li>
-<?php endif ?>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+<?php include("partials/navigation.php") ?>
 
     <!-- Page Content -->
     <div class="container">
---
+++ stuffsharing/partials/navigation.php
@@ -0,0 +1,43 @@
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="./">StuffSharing</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Browse</a>
+                    </li>
+                    <li>
+                        <a href="#">Advertise</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+<?php if ($is_authed): ?>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i> <?=$username?> <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="editprofile.php"><i class="fa fa-cog" aria-hidden="true"></i> Edit Profile</a></li>
+                            <li><a href="logout.php?redirect=main"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
+                        </ul>
+                    </li>
+<?php else: ?>
+                    <li>
+                        <a href="login.php?redirect=main"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
+                    </li>
+<?php endif ?>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
