33a806ec517a43d22ef46572cfe1ad347b6fc871
John Yong <whipermr5@gmail.com>
2016-10-08 15:55:56 +0800

    Fixed login flow and UI for already logged in users visiting login page

--- stuffsharing/login.php
+++ stuffsharing/login.php
@@ -4,34 +4,34 @@ require("include/auth.php");
 
 // Adapted from https://www.phpro.org/tutorials/Basic-Login-Authentication-with-PHP-and-MySQL.html
 
-$success = false;
-
-$username = isset($_POST["username"]) ? $_POST["username"] : "";
-$password = isset($_POST["password"]) ? $_POST["password"] : "";
-
-if (isset($_GET["redirect"])) {
-    switch($_GET["redirect"]) {
-        case "main":
-        $redirect = "./";
-        break;
-
-        case "editprofile":
-        $redirect = "./editprofile.php";
-        break;
-
-        default:
-        $redirect = false;
-    }
-} else {
-    $redirect = false;
-}
-
 if ($is_authed) {
     $success = true;
 
 } else {
+    $success = false;
+
+    $username = isset($_POST["username"]) ? $_POST["username"] : "";
+    $password = isset($_POST["password"]) ? $_POST["password"] : "";
+
+    if (isset($_GET["redirect"])) {
+        switch($_GET["redirect"]) {
+            case "main":
+            $redirect = "./";
+            break;
+
+            case "editprofile":
+            $redirect = "./editprofile.php";
+            break;
+
+            default:
+            $redirect = false;
+        }
+    } else {
+        $redirect = false;
+    }
+
     if (!isset($_POST["login_token"])) {
-        $message = "Please enter a valid username/email and password";
+        $message = "Please enter your details";
 
     } elseif ($_POST["login_token"] != $_SESSION["login_token"]) {
         $message = "Invalid form submission";
@@ -66,15 +66,15 @@ if ($is_authed) {
         }
 
     }
-}
 
-if ($success && $redirect != false) {
-    header('Location: '.$redirect);
-    die();
-}
+    if ($success && $redirect != false) {
+        header('Location: '.$redirect);
+        die();
+    }
 
-$login_token = md5(uniqid('auth', true));
-$_SESSION['login_token'] = $login_token;
+    $login_token = md5(uniqid('auth', true));
+    $_SESSION['login_token'] = $login_token;
+    }
 
 ?>
 <!DOCTYPE html>
@@ -92,7 +92,7 @@ $_SESSION['login_token'] = $login_token;
         <!-- Page Header -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Authentication required</h1>
+                <h1 class="page-header">Authentication page</h1>
             </div>
         </div>
         <!-- /.row -->
@@ -109,25 +109,26 @@ $_SESSION['login_token'] = $login_token;
                         <div class="well well-sm">
 <?php if ($success): ?>
                             You are already logged in as <b><?=$username?></b>. <a href="logout.php?redirect=main">Logout</a>
+                        </div>
 <?php else: ?>
                             <?=$message?>
                         </div>
                         <form method="POST">
                             <div class="form-group">
-                                <label for="username">Username/Email</label>
+                                <label for="username">Username/Email:</label>
                                 <input type="text" class="form-control" id="username" name="username" value="<?=$username?>" maxlength="255" />
                             </div>
                             <div class="form-group">
-                                <label for="password">Password</label>
+                                <label for="password">Password:</label>
                                 <input type="password" class="form-control" id="password" name="password" value="" maxlength="20" />
                             </div>
                             <!-- <div class="form-group"> -->
                                 <input type="hidden" name="login_token" value="<?=$login_token?>" />
-                                <button type="submit" class="btn btn-default pull-right">Login</button>
+                                <button type="submit" class="btn btn-default pull-right"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</button>
                             <!-- </div> -->
                         </form>
-                    </div>
 <?php endif ?>
+                    </div>
                 </div>
             </div>
 
